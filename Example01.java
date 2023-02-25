eae6506fc4b7f3ef1ff8e013b1cdfa16188b1fb7
Valerii_Panov <val250640@mail.ru>
2023-02-25 20:22:05 +1000

    rename

--- Example01.java
+++ Example01.java
@@ -1,7 +1,6 @@
 public class Example01 {
     public static void main(String[] args) {
         int height = 10, width = 10;
-        int maze[][] = GenerateMaze.generate(height, width);
-
+        int maze[][] = GenerateMaze.simple(height, width);
     }
 }
