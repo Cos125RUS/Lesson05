95b953b466942a153df2327803c06f9146ef449a
Valerii_Panov <val250640@mail.ru>
2023-02-26 19:08:34 +1000

    Поменял описание класса в README

--- GenerateMaze.java
+++ GenerateMaze.java
@@ -5,12 +5,14 @@ public class GenerateMaze {
     int height = 10;
     int width = 10;
     int[][] maze;
+    Random random;
 
 
     public GenerateMaze(int height, int width) {
         this.height = height;
         this.width = width;
         this.maze = new int[height][width];
+        this.random = new Random();
     }
 
     /**
@@ -21,8 +23,7 @@ public class GenerateMaze {
      * Возращается двумерный массив maze[height][width]
      */
     public int[][] simple() {
-        Random random = new Random();
-        boards(maze, height, width, -1);
+        boards(maze, -1);
 
         for (int i = 1; i < width - 1; i++)
             for (int j = 0; j < height - 1; j++) {
@@ -44,11 +45,11 @@ public class GenerateMaze {
      */
     public void euler(){
         int[][] sets = new int[height][width];
-        boards(maze, height, width, -1);
-        boards(sets, height, width, 1);
+        boards(maze, -1);
+        boards(sets, 1);
 
         for (int i = 1; i < height-1; i++){
-            fillSetLine(maze, sets, width-1, i);
+            fillSetLine(sets, width-1, i);
             System.out.println();
         }
 
@@ -65,21 +66,20 @@ public class GenerateMaze {
      * Метод создания границ лабиринта
      * @param height высота лабиринта
      * @param width ширина лабиринта
-     * @param maze лабиринт в виде массива int[][]
+     * @param map лабиринт в виде массива int[][]
      */
-    private static void boards(int[][] maze, int height, int width, int wall){
+    private void boards(int[][] map, int wall){
         for (int i = 0; i < width; i++) {
-            maze[0][i] = wall;
-            maze[height - 1][i] = wall;
+            map[0][i] = wall;
+            map[height - 1][i] = wall;
         }
         for (int i = 1; i < height - 1; i++) {
-            maze[i][0] = wall;
-            maze[i][width - 1] = wall;
+            map[i][0] = wall;
+            map[i][width - 1] = wall;
         }
     }
 
-    private static void fillSetLine(int[][] maze, int[][] sets, int width, int line) {
-        Random random = new Random();
+    private void fillSetLine(int[][] sets, int width, int line) {
         sets[line][1] = 1;
         System.out.print(sets[line][1] + " ");
         for (int j = 2; j < width - 1; j++) {
@@ -97,13 +97,9 @@ public class GenerateMaze {
         }
         sets[line][width - 1] = sets[line][width - 2];
         System.out.print(sets[line][width - 1] + " ");
-
-
-
     }
 
-    private static void buildWall(int[][] maze, int[][] sets, int width, int line){
-        Random random = new Random();
+    private void buildWall(int[][] sets, int width, int line){
         int countWall = 0;
         int countSet = 1;
         for (int j = 1; j < width-1; j++){
