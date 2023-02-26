4fdd6a92d59f4da26548efd32f3e1f62f0ef2b4e
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2023-02-26 22:40:40 +0700

    В класс Showway добавил обозначения начала и конца пути.

--- ShowWay.java
+++ ShowWay.java
@@ -2,8 +2,12 @@ public class ShowWay {
     
     // метод который присваивает всем ячейкам map, которые являются путем следования значение =-2 
     public static void showWay(int[][] map, Point2D[] point){
-        for (int i = 0; i < point.length; i++){
+        // точка конечная
+        map[point[0].x][point[0].y] = -4;     
+        for (int i = 1; i < point.length - 1; i++){
             map[point[i].x][point[i].y] = -2;
         }
+        // точка начальная
+        map[point[point.length - 1].x][point[point.length - 1].y] = -3; 
     }
 }
