c5455465b81eb6f35f76c91e2bdbcaffcfee0edb
Valerii_Panov <val250640@mail.ru>
2023-02-27 20:02:51 +1000

    euler

--- GenerateMaze.java
+++ GenerateMaze.java
@@ -48,28 +48,16 @@ public class GenerateMaze {
     public void euler() {
         int[][] sets = new int[height][width];
         boards(maze, -1);
-        boards(sets, 1);
 
-        fillSetLine(sets, width - 1, 1);
-        if (height > 3)
-            buildWall(sets, width - 1, 1);
-        for (int i = 3; i < height - 1; i += 2) {
-            fillSetLine(sets, width - 1, i);
+        for (int i = 1; i < height - 1; i += 2) {
+            fillSetLine(i);
             if (i < height - 2)
-                buildWall(sets, width - 1, i);
+                buildWall(i);
         }
-//        for (int i = 1; i < height - 2; i+=2) {
-//            cross(width - 1, i);
-//        }
-//        maze[height - 2][1] = 0;
-//        maze[height - 2][width - 2] = 0;
 
-//        for (int i = 0; i < height; i++){
-//            for (int j = 0; j < width; j++)
-//                System.out.print(sets[i][j] + " ");
-//            System.out.println();
-//        }
-//        System.out.println("\n");
+        for (int i = 2; i < width - 2; i+=2)
+            if (maze[height - 1][i] == -1 && maze[height - 2][i-1] == -1 && maze[height - 1][i+1] == -1)
+                maze[height - 1][i] = 0;
     }
 
 
@@ -90,50 +78,35 @@ public class GenerateMaze {
         }
     }
 
-    private void fillSetLine(int[][] sets, int width, int line) {
-        sets[line][1] = 1;
-        for (int j = 2; j < width - 1; j++)
-            if (maze[line-1][j] == 0 && maze[line-1][j - 1] == -1 && maze[line-1][j + 1] == -1)
-                sets[line][j] = sets[line][j-1];
-            else if (maze[line-1][j] == 0 && maze[line-1][j - 1] == 0)
-                brick(sets, width, line, j++);
-            else if (random.nextInt(2) == 0)
-                sets[line][j] = sets[line][j - 1];
-            else
-                brick(sets, width, line, j++);
-
-        sets[line][width - 1] = sets[line][width - 2];
+    private void fillSetLine(int line) {
+        for (int j = 2; j < width - 2; j += 2)
+            if (maze[line - 1][j] == 0 && maze[line - 1][j + 2] == 0) {
+                maze[line][j] = -1;
+                maze[line - 1][j] = -1;
+                maze[line + 1][j] = -1;
+            } else if (random.nextInt(3) == 0) {
+                maze[line][j] = -1;
+                maze[line + 1][j] = -1;
+            }
     }
 
-    private void buildWall(int[][] sets, int width, int line) {
+    private void buildWall(int line) {
         int countWall = 0;
-        int countSet = 1;
-        for (int j = 1; j < width - 1; j++) {
-            if (sets[line][j] != sets[line][j + 1]) {
+        int countSet = 0;
+        for (int j = 2; j < width - 2; j += 2) {
+            if (maze[line][j] == -1) {
                 if (countSet == countWall)
-                    maze[line + 1][j - random.nextInt(countSet)] = 0;
+                    maze[line + 1][j - 1 - random.nextInt(1 + countSet) * 2] = 0;
                 countWall = 0;
-                countSet = 1;
+                countSet = 0;
             } else {
                 countSet++;
+                maze[line + 1][j] = -1;
                 if (random.nextInt(2) == 0) {
-                    maze[line + 1][j] = -1;
+                    maze[line + 1][j - 1] = -1;
                     countWall++;
                 }
             }
         }
-    }
-
-    private void cross(int width, int line) {
-        for (int j = 2; j < width - 2; j++)
-            if (maze[line][j] == -1 && (maze[line + 1][j + 1] == -1 || maze[line + 1][j - 1] == -1))
-                maze[line + 1][j] = -1;
-    }
-
-    private void brick(int[][] sets, int width, int line, int j) {
-        maze[line][j] = -1;
-        sets[line][j] = sets[line][j - 1] + 1;
-        if (j != width - 2)
-            sets[line][j + 1] = sets[line][j];
     }
 }
